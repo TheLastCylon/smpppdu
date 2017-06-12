0593ecffb2cd3283f53709f09afaa6eace8f6b88
Dirk Botha <bothadj@gmail.com>
2017-06-12 12:21:44 +0200

    Fixed include paths in smpppdu_all.hpp

--- smpppdu_all.hpp
+++ smpppdu_all.hpp
@@ -1,73 +1,73 @@
-// File  : smpp_pdu_all.hpp
+// File  : smpppdu_all.hpp
 // Author: Dirk J. Botha <bothadj@gmail.com>
 //
-// This file is part of smpp_pdu library.
+// This file is part of smpppdu library.
 //
-// The smpp_pdu library is free software: you can redistribute it and/or modify
+// The smpppdu library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The smpp_pdu library is distributed in the hope that it will be useful,
+// The smpppdu library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the smpp_pdu library. If not, see <http://www.gnu.org/licenses/>.
+// along with the smpppdu library. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef _SMPP_PDU_ALL_HPP_
 #define _SMPP_PDU_ALL_HPP_
 
-#include <smpp_pdu/bind_type.hpp>
-#include <smpp_pdu/bind_type_resp.hpp>
-#include <smpp_pdu/command_id.hpp>
-#include <smpp_pdu/command_length.hpp>
-#include <smpp_pdu/command_status.hpp>
-#include <smpp_pdu/endian_util.hpp>
-#include <smpp_pdu/error.hpp>
-#include <smpp_pdu/header_pdu.hpp>
-#include <smpp_pdu/lexical_cast.hpp>
-#include <smpp_pdu/octet_array.hpp>
-#include <smpp_pdu/octet.hpp>
-#include <smpp_pdu/sequence_number.hpp>
-#include <smpp_pdu/smpp_pdu.hpp>
-#include <smpp_pdu/standard_parameters.hpp>
-#include <smpp_pdu/tlv.hpp>
-#include <smpp_pdu/tlv_list.hpp>
-#include <smpp_pdu/alert_notification.hpp>
-#include <smpp_pdu/bind_receiver.hpp>
-#include <smpp_pdu/bind_receiver_resp.hpp>
-#include <smpp_pdu/bind_transceiver.hpp>
-#include <smpp_pdu/bind_transceiver_resp.hpp>
-#include <smpp_pdu/bind_transmitter.hpp>
-#include <smpp_pdu/bind_transmitter_resp.hpp>
-#include <smpp_pdu/broadcast_sm.hpp>
-#include <smpp_pdu/broadcast_sm_resp.hpp>
-#include <smpp_pdu/cancel_broadcast_sm.hpp>
-#include <smpp_pdu/cancel_broadcast_sm_resp.hpp>
-#include <smpp_pdu/cancel_sm.hpp>
-#include <smpp_pdu/cancel_sm_resp.hpp>
-#include <smpp_pdu/data_sm.hpp>
-#include <smpp_pdu/data_sm_resp.hpp>
-#include <smpp_pdu/deliver_sm.hpp>
-#include <smpp_pdu/deliver_sm_resp.hpp>
-#include <smpp_pdu/enquire_link.hpp>
-#include <smpp_pdu/enquire_link_resp.hpp>
-#include <smpp_pdu/generic_nack.hpp>
-#include <smpp_pdu/outbind.hpp>
-#include <smpp_pdu/query_broadcast_sm.hpp>
-#include <smpp_pdu/query_broadcast_sm_resp.hpp>
-#include <smpp_pdu/query_sm.hpp>
-#include <smpp_pdu/query_sm_resp.hpp>
-#include <smpp_pdu/replace_sm.hpp>
-#include <smpp_pdu/replace_sm_resp.hpp>
-#include <smpp_pdu/submit_multi.hpp>
-#include <smpp_pdu/submit_multi_resp.hpp>
-#include <smpp_pdu/submit_sm.hpp>
-#include <smpp_pdu/submit_sm_resp.hpp>
-#include <smpp_pdu/unbind.hpp>
-#include <smpp_pdu/unbind_resp.hpp>
+#include <smpppdu/bind_type.hpp>
+#include <smpppdu/bind_type_resp.hpp>
+#include <smpppdu/command_id.hpp>
+#include <smpppdu/command_length.hpp>
+#include <smpppdu/command_status.hpp>
+#include <smpppdu/endian_util.hpp>
+#include <smpppdu/error.hpp>
+#include <smpppdu/header_pdu.hpp>
+#include <smpppdu/lexical_cast.hpp>
+#include <smpppdu/octet_array.hpp>
+#include <smpppdu/octet.hpp>
+#include <smpppdu/sequence_number.hpp>
+#include <smpppdu/smpppdu.hpp>
+#include <smpppdu/standard_parameters.hpp>
+#include <smpppdu/tlv.hpp>
+#include <smpppdu/tlv_list.hpp>
+#include <smpppdu/alert_notification.hpp>
+#include <smpppdu/bind_receiver.hpp>
+#include <smpppdu/bind_receiver_resp.hpp>
+#include <smpppdu/bind_transceiver.hpp>
+#include <smpppdu/bind_transceiver_resp.hpp>
+#include <smpppdu/bind_transmitter.hpp>
+#include <smpppdu/bind_transmitter_resp.hpp>
+#include <smpppdu/broadcast_sm.hpp>
+#include <smpppdu/broadcast_sm_resp.hpp>
+#include <smpppdu/cancel_broadcast_sm.hpp>
+#include <smpppdu/cancel_broadcast_sm_resp.hpp>
+#include <smpppdu/cancel_sm.hpp>
+#include <smpppdu/cancel_sm_resp.hpp>
+#include <smpppdu/data_sm.hpp>
+#include <smpppdu/data_sm_resp.hpp>
+#include <smpppdu/deliver_sm.hpp>
+#include <smpppdu/deliver_sm_resp.hpp>
+#include <smpppdu/enquire_link.hpp>
+#include <smpppdu/enquire_link_resp.hpp>
+#include <smpppdu/generic_nack.hpp>
+#include <smpppdu/outbind.hpp>
+#include <smpppdu/query_broadcast_sm.hpp>
+#include <smpppdu/query_broadcast_sm_resp.hpp>
+#include <smpppdu/query_sm.hpp>
+#include <smpppdu/query_sm_resp.hpp>
+#include <smpppdu/replace_sm.hpp>
+#include <smpppdu/replace_sm_resp.hpp>
+#include <smpppdu/submit_multi.hpp>
+#include <smpppdu/submit_multi_resp.hpp>
+#include <smpppdu/submit_sm.hpp>
+#include <smpppdu/submit_sm_resp.hpp>
+#include <smpppdu/unbind.hpp>
+#include <smpppdu/unbind_resp.hpp>
 
 #endif // _SMPP_PDU_ALL_HPP_
 
